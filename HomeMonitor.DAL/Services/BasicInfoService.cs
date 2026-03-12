c0616d9731658087a562b9738e1323f40a8a0366
Tomáš Luňák <tlunak@post.cz>
2026-03-12 14:02:36 +0100

    aktualizace knihovny restclient

--- HomeMonitor.DAL/Services/BasicInfoService.cs
+++ HomeMonitor.DAL/Services/BasicInfoService.cs
@@ -7,17 +7,22 @@ using System.Text;
 
 namespace HomeMonitor.DAL.Services
 {
-    public class BasicInfoService
+  public class BasicInfoService
+  {
+    public BasicInfo GetBasicInfo()
     {
-        public BasicInfo GetBasicInfo()
-        {
-            var client = new RestClient("http://85.160.71.134:6999");
-            client.Authenticator = new HttpBasicAuthenticator("api", "basicauth");
-            var request = new RestRequest("TecoApi/GetObject?ApiSimple", Method.GET);
-            IRestResponse<List<RootInfo>> response = client.Execute<List<RootInfo>>(request);
-                        
-            RootInfo rootInfo = Newtonsoft.Json.JsonConvert.DeserializeObject<RootInfo>(response.Content);
-            return rootInfo.ApiSimple;
-        }
+      var options = new RestClientOptions("http://85.160.71.134:6999")
+      {
+        Authenticator = new HttpBasicAuthenticator("api", "basicauth")
+      };
+
+      var client = new RestClient(options);
+
+      var request = new RestRequest("TecoApi/GetObject?ApiSimple", Method.Get);
+      RestResponse<List<RootInfo>> response = client.Execute<List<RootInfo>>(request);
+
+      RootInfo rootInfo = Newtonsoft.Json.JsonConvert.DeserializeObject<RootInfo>(response.Content);
+      return rootInfo.ApiSimple;
     }
+  }
 }
